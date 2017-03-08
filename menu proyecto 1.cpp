5a0aa3ea02c6c1341372db85a9e9df6d4ac7e6cf
Unknown <kelvinprog8@gmail.com>
2017-03-08 11:56:05 -0400

    inicio
    inicio del proyecto

---
+++ menu proyecto 1.cpp
@@ -0,0 +1,15 @@
+/*
+* Lenguaje de programacion 2
+* Bachileres: Azuaje Kelvin CI: 25799617 / Rosales Zahiri CI: 25450708
+* Sistemas S51
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
